d7b5d0d06e7f3f0915a09a383b6b7949acd35916
ras1k <rasikabdullah@gmail.com>
2023-03-22 02:03:45 +0600

    map added

---
+++ chain.js
@@ -0,0 +1,9 @@
+const users = [
+    {
+        id: 1,
+        name: 'Karim',
+        lob: 'Doctor'
+    }
+]
+
+console.log(users[0].name);
---
+++ map.js
@@ -0,0 +1,4 @@
+const numbers = [2, 4, 6, 8, 10];
+const doubleIt = num => num*2;
+const double = numbers.map(doubleIt);
+console.log(double);
